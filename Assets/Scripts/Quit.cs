eb52e8086377584536df00146022e149ef95e257
jciecielag <jakubciecielag@o2.pl>
2024-01-01 23:52:17 +0100

    Main Menu
    Main Menu UI

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Quit : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void NewLvl()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LevelSelector()
+    {
+        
+    }
+
+
+
+}
